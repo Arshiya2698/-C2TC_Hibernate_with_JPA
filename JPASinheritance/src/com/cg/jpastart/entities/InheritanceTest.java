dc42f6fb67aa3cae3745e21ecd241e75142093fb
Arshiya <Arshiya@DESKTOP-HPC1H2G>
2022-08-23 14:51:12 +0530

    first commit

---
+++ JPASinheritance/src/com/cg/jpastart/entities/InheritanceTest.java
@@ -0,0 +1,30 @@
+package com.cg.jpastart.entities;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+public class InheritanceTest {
+	public static void main(String args[]) {
+		EntityManagerFactory factory=Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em=factory.createEntityManager();
+		em.getTransaction().begin();
+//		create one employee
+		Employee employee=new Employee();
+		employee.setName("shahid");
+		employee.setSalary(5000);
+		em.persist(employee);
+		//create one manager
+				Manager manager = new Manager();
+				manager.setName("harry");
+				manager.setSalary(8000);
+				manager.setDepartmentName("Sales");
+				em.persist(manager);
+		
+		em.getTransaction().commit();
+		
+		
+		em.close();
+		factory.close();
+		System.out.println("Added one employee and manager to database.");
+	}
+
+}
